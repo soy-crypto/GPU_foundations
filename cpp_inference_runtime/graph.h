393093835b1986fcedf6f4222a2899371f4deead
peterzhu <ztj_nwu@snnu.edu.cn>
2026-03-08 17:46:43 -0700

    0308

--- cpp_inference_runtime/graph.h
+++ cpp_inference_runtime/graph.h
@@ -2,11 +2,10 @@
 #include <vector>
 #include "ops.h"
 
-class Graph {
+class Graph 
+{
 public:
     std::vector<Operator*> ops;
-
     void add_op(Operator* op);
-
     Tensor run(const Tensor& input);
 };

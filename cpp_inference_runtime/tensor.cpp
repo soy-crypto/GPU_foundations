0c0d5e2cff7f2a9b0017220435f2cb904cadbf9c
peterzhu <ztj_nwu@snnu.edu.cn>
2026-03-11 17:24:11 -0700

    0311

--- cpp_inference_runtime/tensor.cpp
+++ cpp_inference_runtime/tensor.cpp
@@ -1,12 +1,15 @@
 #include "tensor.h"
 
+//constructor
 Tensor::Tensor(int r, int c) : rows(r), cols(c), data(r * c, 0.0f) {}
 
+//operator
 float& Tensor::operator()(int r, int c) 
 {
     return data[r * cols + c];
 }
 
+//const operator
 float Tensor::operator()(int r, int c) const 
 {
     return data[r * cols + c];

807cf6bc9fddf74f8a81afb231580131c8820640
peter zhu <ztjnwu@gmail.com>
2026-03-18 01:43:54 +0000

    0317

--- cpp_inference_runtime/mini_inference_runtime_cpu.cpp
+++ cpp_inference_runtime/mini_inference_runtime_cpu.cpp
@@ -9,49 +9,30 @@
 ////////////////////////////////////////////////////////////
 class Tensor
 {
-    private:
-        std::vector<float> data;
-        int rows;
-        int cols;
+private:
+    std::vector<float> data;
+    int rows;
+    int cols;
 
-    public:
-        Tensor(int r, int c): rows(r), cols(c), data(r * c, 0.0f) {}
+public:
+    Tensor(int r, int c) : rows(r), cols(c), data(r * c, 0.0f) {}
 
-        float& operator()(int r, int c)
-        {
-            return data[r * cols + c];
-        }
+    float& operator()(int r, int c)
+    {
+        return data[r * cols + c];
+    }
 
-        float operator()(int r, int c) const
-        {
-            return data[r * cols + c];
-        }
+    float operator()(int r, int c) const
+    {
+        return data[r * cols + c];
+    }
 
-        float* getData()
-        {
-            return data.data();
-        }
+    float* getData() { return data.data(); }
+    const float* getData() const { return data.data(); }
 
-        const float* getData() const
-        {
-            return data.data();
-        }
-
-        int getRows() const
-        {
-            return rows;
-        }
-
-        int getCols() const
-        {
-            return cols;
-        }
-
-        int getSize() const
-        {
-            return rows * cols;
-        }
-
+    int getRows() const { return rows; }
+    int getCols() const { return cols; }
+    int getSize() const { return rows * cols; }
 };
 
 ////////////////////////////////////////////////////////////
@@ -60,61 +41,68 @@ class Tensor
 
 class Operator
 {
-    public:
-        virtual ~Operator() {}
-        virtual Tensor forward(const Tensor& input) = 0;
+public:
+    virtual ~Operator() {}
+    virtual Tensor forward(const Tensor& input) = 0;
 };
+
+////////////////////////////////////////////////////////////
+// ReLU
+////////////////////////////////////////////////////////////
 
 class ReLU : public Operator
 {
-    public:
-        Tensor forward(const Tensor& input) override
+public:
+    Tensor forward(const Tensor& input) override
+    {
+        Tensor output(input.getRows(), input.getCols());
+
+        const float* in = input.getData();
+        float* out = output.getData();
+
+        for (int i = 0; i < input.getSize(); i++)
         {
-            Tensor output = Tensor(input.getRows(), input.getCols());
-
-            const float* inputData = input.getData();
-            float* outData = output.getData();
-
-            for (int i = 0; i < input.getSize(); i++)
-            {
-                outData[i] = std::max(0.0f, inputData[i]);
-            }
-
-            return output;
+            out[i] = std::max(0.0f, in[i]);
         }
 
+        return output;
+    }
 };
+
+////////////////////////////////////////////////////////////
+// Softmax
+////////////////////////////////////////////////////////////
 
 class Softmax : public Operator
 {
-    public:
-        Tensor forward(const Tensor& input) override
+public:
+    Tensor forward(const Tensor& input) override
+    {
+        Tensor output(input.getRows(), input.getCols());
+
+        const float* in = input.getData();
+        float* out = output.getData();
+
+        float maxVal = in[0];
+        for (int i = 1; i < input.getSize(); i++)
         {
-            Tensor output = Tensor(input.getRows(), input.getCols());
-            const float* inputData = input.getData();
-            float* outData = output.getData();
-
-            float maxVal = inputData[0];
-            for (int i = 1; i < input.getSize(); i++)
-            {
-                maxVal = std::max(maxVal, inputData[i]);
-            }
-
-            float sum = 0.0f;
-            for (int i = 0; i < input.getSize(); i++)
-            {
-                outData[i] = std::exp(inputData[i] - maxVal);
-                sum += outData[i];
-            }
-
-            for (int i = 0; i < input.getSize(); i++)
-            {
-                outData[i] /= sum;
-            }
-
-            //Return
-            return output;
+            maxVal = std::max(maxVal, in[i]);
         }
+
+        float sum = 0.0f;
+        for (int i = 0; i < input.getSize(); i++)
+        {
+            out[i] = std::exp(in[i] - maxVal);
+            sum += out[i];
+        }
+
+        for (int i = 0; i < input.getSize(); i++)
+        {
+            out[i] /= sum;
+        }
+
+        return output;
+    }
 };
 
 ////////////////////////////////////////////////////////////
@@ -123,43 +111,43 @@ class Softmax : public Operator
 
 class Graph
 {
-    private:
-        std::vector<Operator*> ops;
+private:
+    std::vector<Operator*> ops;
 
-    public:
-        void add_op(Operator* op)
+public:
+    void add_op(Operator* op)
+    {
+        ops.push_back(op);
+    }
+
+    Tensor run(const Tensor& input)
+    {
+        Tensor x = input;
+
+        for (const auto* op : ops)
         {
-            ops.push_back(op);
+            Tensor out = op->forward(x);
+            x = std::move(out);   // 🔥 move instead of copy
         }
 
-        Tensor run(const Tensor& input)
-        {
-            Tensor x = input;
-            for (auto op : ops)
-            {
-                x = op->forward(x);
-            }
-
-            return x;
-        }
-
+        return x;
+    }
 };
 
 ////////////////////////////////////////////////////////////
-// Main Runtime
+// Main
 ////////////////////////////////////////////////////////////
 
 int main()
 {
-    // Init input tensor
-    Tensor input = Tensor(1,3);
+    Tensor input(1, 3);
     float* data = input.getData();
-    for(int i = 0;i < input.getSize(); i++)
+
+    for (int i = 0; i < input.getSize(); i++)
     {
         data[i] = static_cast<float>(i);
     }
 
-    // Build computation graph
     Graph graph;
     ReLU relu;
     Softmax softmax;
@@ -167,24 +155,23 @@ int main()
     graph.add_op(&relu);
     graph.add_op(&softmax);
 
-    // Run inference
     auto start = std::chrono::high_resolution_clock::now();
     Tensor output = graph.run(input);
     auto end = std::chrono::high_resolution_clock::now();
 
-    // Print output
+    double latency =
+        std::chrono::duration<double, std::milli>(end - start).count();
+
     std::cout << "Output: ";
-    double latency = std::chrono::duration<double,std::milli>(end - start).count();
     float* out = output.getData();
-    for(int i=0;i<output.getSize();i++)
+    for (int i = 0; i < output.getSize(); i++)
     {
         std::cout << out[i] << " ";
     }
 
-    std::cout << std::endl;
-    std::cout << "Latency: " << latency << " ms\n";
+    std::cout << "\nLatency: " << latency << " ms\n";
     std::cout << "Inference done\n";
 
-    //Return
     return 0;
+    
 }
